592e755bd2cdc0d518662726ab6f825583b30291
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2017-07-11 17:09:00 +0200

    Add PositiveOverNegativeCountTrait

---
+++ src/PositiveOverNegativeCountTrait.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of PHPUnit Good Practices.
+ *
+ * (c) Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PHPUnitGoodPractices;
+
+trait PositiveOverNegativeCountTrait
+{
+    // TODO:
+    //    public static function assertCount($expectedCount, $haystack, $message = '')
+    //    public static function assertAttributeCount($expectedCount, $haystackAttributeName, $haystackClassOrObject, $message = '')
+    //    public static function assertNotCount($expectedCount, $haystack, $message = '')
+    //    public static function assertAttributeNotCount($expectedCount, $haystackAttributeName, $haystackClassOrObject, $message = '')
+}
