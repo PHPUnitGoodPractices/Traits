3fed56ca567af763d34ea18d9f94f2e1356d264f
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2017-07-17 22:54:30 +0200

    AutoReviewTest - ensure PHPUnit version is not overwritten

---
+++ tests/AutoReviewTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of PHPUnit Good Practices.
+ *
+ * (c) Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PHPUnitGoodPractices\Tests;
+
+use PHPUnit\Framework\Error\Warning;
+use PHPUnit\Framework\TestCase;
+use PHPUnitGoodPractices\IdentityOverEqualityTrait;
+use PHPUnitGoodPractices\Reporter;
+
+/**
+ * @coversNothing
+ */
+final class AutoReviewTest extends TestCase
+{
+    public function testThatThereIsProperPHPUnitInComposer()
+    {
+        $json = json_decode(file_get_contents(__DIR__.'/../composer.json'), true);
+
+        $this->assertSame(
+            '^4 || ^5 || ^6',
+            $json['require']['phpunit/phpunit']
+        );
+    }
+}
