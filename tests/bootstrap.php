cda2856b64db87d6ef1f3cfedda5476d1accbfb7
Dariusz Rumiński <dariusz.ruminski@gmail.com>
2017-07-17 22:32:06 +0200

    Add IdentityOverEqualityTrait, remove StrictAssertionTrait (#5)
    * rename trait
    
    * TODO
    
    * resolve todo
    
    * fix after rebase
    
    * fix v4
    
    * fix comment
    
    * redesign tests, more tests thanks to using data provider
    
    * php downgrade
    
    * test undef 5.4 as well
    
    * fix phpunit 6
    
    * fix
    
    * docs

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,6 +13,10 @@ if (!class_exists('PHPUnit\Framework\Error\Warning')) {
     class_alias('PHPUnit_Framework_Error_Warning', 'PHPUnit\Framework\Error\Warning');
 }
 
+if (!class_exists('PHPUnit\Framework\ExpectationFailedException')) {
+    class_alias('PHPUnit_Framework_ExpectationFailedException', 'PHPUnit\Framework\ExpectationFailedException');
+}
+
 if (!class_exists('PHPUnit\Framework\TestCase')) {
     class_alias('PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
 }
